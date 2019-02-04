294c526caedd2a9c59db73538a8c04e428913066
Lucas <DicePhrase@users.noreply.github.com>
2019-02-04 02:09:34 -0800

    build.js: make packages for browsers, auto-fix manifest.json for Firefox

--- BUILD.md
+++ BUILD.md
@@ -1,14 +1,22 @@
 # Building DicePhrase
 
-DicePhrase's Help page in the `doc` directory needs to be built using Jekyll.
+The Firefox extension needs to be packaged with Web-ext. Also, DicePhrase's Help page in the `doc` directory needs to be built using Jekyll.
 
-1. Install [Jekyll](https://jekyllrb.com/).
-2. Clone the repository:
+1. Install [Jekyll](https://jekyllrb.com/):
+    ```
+    gem install jekyll
+    ```
+2. Install [Web-ext](https://www.npmjs.com/package/web-ext):
+    ```
+    npm install --global web-ext
+    ```
+3. Clone the repository:
     ```
     git clone https://github.com/dicephrase/dicephrase.git
     cd dicephrase
     ```
-3. Run the build script:
+4. Run the build script:
     ```
     sh ./build.sh
     ```
+5. Packaged files will be located in the 'pkg' directory.
--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # Fail and exit.
 fail () {
@@ -13,14 +13,54 @@ if command -v jekyll 2>/dev/null; then
 	# Jekyll is installed.
 	jekyll clean --source ./doc --destination ./doc/_site || fail "`jekyll clean` failed."
 	jekyll build --source ./doc --destination ./doc/_site || fail "`jekyll build` failed."
-	echo "The Jekyll build is done."
+	echo "Done building documentation."
 else
-	fail "Jekyll is required but it's not installed. To install Jekyll, see https://jekyllrb.com/."
+	fail "Jekyll is required but is not installed. To install Jekyll, see https://jekyllrb.com/."
 fi
+
+# Prepare package files for Chrome and Firefox in /pkg.
+echo "Preparing package files..."
+rm -rf pkg
+mkdir -p pkg/chrome pkg/firefox
+rsync -r --exclude "pkg" --exclude ".git*" ./ pkg/chrome/
+rsync -r --exclude "pkg" --exclude ".git*" ./ pkg/firefox/
+
+# Edit manifest.json for Firefox compatibility.
+echo "Editing manifest.json for Firefox compatibility..."
+# Remove "offline_enabled" key, Firefox doesn't support it.
+# https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/manifest.json/offline_enabled
+sed -i "/\"offline_enabled\"/d" pkg/firefox/manifest.json
+# Remove "sandbox" key, Firefox doesn't support it.
+# https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/manifest.json
+sed -i '\%"sandbox"%,\%},%d' pkg/firefox/manifest.json
+# Change "incognito" key, Firefox only supports "spanning" value.
+# https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/manifest.json/incognito
+sed -i "s/\"incognito\": \"split\"/\"incognito\": \"spanning\"/" pkg/firefox/manifest.json
+# Remove "persistent" property from "background" key, Firefox doesn't support it.
+# https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/manifest.json/background
+sed -i "s/\"page\": \"event-page.html\",/\"page\": \"event-page.html\"/" pkg/firefox/manifest.json
+sed -i "/\"persistent\"/d" pkg/firefox/manifest.json
+echo "Done editing manifest.json."
+
+# Package Chrome extension.
+echo "Packaging Chrome extension..."
+pushd pkg/chrome
+zip -r ../dicephrase-chrome.zip ./
+popd
+echo "Done packaging Chrome extension."
+# Package Firefox extension.
+echo "Packaging Firefox extension..."
+if command -v web-ext 2>/dev/null; then
+	web-ext build --source-dir pkg/firefox --artifacts-dir pkg
+	echo "Done packaging Firefox extension."
+else
+	fail "Web-ext is required but is not installed. To install web-ext, see https://www.npmjs.com/package/web-ext."
+fi
+echo "Done packaging."
 
 # List files that contain version numbers.
 echo "These files may contain version numbers that need to be manually updated for a new release:"
-grep -irlE --exclude="*.svg" --exclude-dir="fw" --exclude-dir=".git" "version.+[0-9]" ./ || fail
+grep -irlE --exclude="*.svg" --exclude-dir="fw" --exclude-dir=".git" --exclude-dir="pkg" "version.+[0-9]" ./ || fail
 
 echo "The build completed successfully."
 exit 0
